14f216222770d8a487d8a916aef2e4c08a18c5a0
Vladislav <molkov.udmnn@gmail.com>
2023-02-15 16:09:47 +0300

    add method printMessage() and private string variable called message in Main class

--- src/main/java/org/qaGuru/Main.java
+++ src/main/java/org/qaGuru/Main.java
@@ -1,7 +1,11 @@
 package org.qaGuru;
 
 public class Main {
+    private String message = "Hello world!";
+    public void printMessage(){
+        System.out.println(message);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().printMessage();
     }
 }
